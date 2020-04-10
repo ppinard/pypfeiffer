de8ace9b6c010ae31c7327f3d25e3e6fdb171109
Philippe Pinard <philippe.pinard@gmail.com>
2020-04-10 12:30:33 +0100

    Add dummy test

---
+++ tests/test_interface.py
@@ -0,0 +1,14 @@
+""""""
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+from pypfeiffer.interface import PfeifferSingleGaugeInterface
+
+# Globals and constants variables.
+
+def test_import():
+    PfeifferSingleGaugeInterface()
+    
